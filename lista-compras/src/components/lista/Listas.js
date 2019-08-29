03c53fd02918d9fa0676b91d782d3e58f687bbf3
milena <milenadesouzacamargo@gmail.com>
2019-08-29 19:58:38 -0400

    Estilizando a tela de listas

---
+++ lista-compras/src/components/lista/Listas.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import Menu from '../menu/Menu';
+import logo from '../../img/logo.png';
+import ListaService from '../../services/ListaService';
+import './Lista.scss';
+import adicionar from '../../img/adicionar.png';
+
+export default class Listas extends Component {
+
+    constructor() {
+        /**
+         * Toda vez que criar um construtor
+         * em um componente React, lembre-se
+         * de invocar o construtor da classe
+         * mãe Component na primeira linha de
+         * código.
+         */
+        super();
+
+        this.state = {
+            listas: []
+        }
+
+        this.service = new ListaService();
+    }
+
+   async componentDidMount() {
+        const listas =
+            await this.service.recuperarListas();
+        this.setState({ listas });
+    }
+
+    render() {
+        const listas =
+            this.state.listas.map(lista => (
+                <div className = "item" key={lista._id}>{lista.nome}</div>
+            ));
+
+        return (
+            <div>
+                <Menu
+                    logo={logo}
+                    paginaAnterior="/"
+                    titulo="Lista de Compras" />
+
+                <div className="conteiner">
+                    <div>
+                        <h2>Minhas listas</h2>
+
+                        <div id = "listagem">
+                            {listas}
+                        </div>
+
+                        <div id="areaBotao">
+                            <div id = "botaoNovaLista">
+                                 <img src = {adicionar} alt = "Nova Lista" id = "botaoNovaLista"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ lista-compras/src/services/ListaService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class ListaService {
+
+    constructor() {
+        this.listas = [];
+        this.api   = axios.create({
+            baseURL:'https:\\secret-plateau-95576.herokuapp.com'
+        });
+    }
+
+    async recuperarListas() {
+        let resposta = await this.api.get('/listas');
+        this.listas = resposta.data;
+        return this.listas;
+    }
+
+}
+
+
